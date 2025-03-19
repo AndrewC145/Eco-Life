8a85b8604e38c8e76f8be4f85b61c0d9f528bf8e
Andrew Cao <andrewcao618@gmail.com>
2025-03-19 17:30:10 -0400

    Started working on range min and max

--- js/calculator.js
+++ js/calculator.js
@@ -3,6 +3,8 @@ const output = document.querySelector('.output');
 const prevButton = document.querySelector('.prev-button');
 const nextButton = document.querySelector('.next-button');
 const question = document.querySelector('.question');
+const rangeMin = document.querySelector('.range-left');
+const rangeMax = document.querySelector('.range-right');
 const type = document.querySelector('.type');
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -41,11 +43,15 @@ function changeType() {
   }
 }
 
+function changeValues() {}
+
 function moveNext() {
   if (count < questions.length - 1) {
     count++;
     question.textContent = questions[count].question;
     slider.value = questions[count].answer;
+    rangeMin.textContent = questions[count].minVal;
+    rangeMax.textContent = questions[count].maxVal;
     showRangeValue();
   }
 
@@ -73,38 +79,53 @@ nextButton.addEventListener('click', () => {
   moveNext();
 });
 
-// sample questions for now
 const questions = [
   {
     question: 'How many kilometers do you drive each week?',
+    minVal: 0,
+    maxVal: 600,
     answer: slider.value,
   },
   {
-    question: 'How often do you take public transportation',
+    question: 'How far do you take public transportation ',
+    minVal: 0,
+    maxVal: 600,
     answer: slider.value,
   },
   {
     question: 'How often do you carpool?',
+    minVal: 0,
+    maxVal: 100,
     answer: slider.value,
   },
   {
     question: 'How often do you eat animal-based products?',
+    minVal: 0,
+    maxVal: 100,
     answer: slider.value,
   },
   {
     question: 'What percentage of your food is locally sourced or organic?',
+    minVal: 0,
+    maxVal: 100,
     answer: slider.value,
   },
   {
     question: 'How many people live in your house?',
+    minVal: 0,
+    maxVal: 10,
     answer: slider.value,
   },
   {
     question: 'What is the size of your house?',
+    minVal: 0,
+    maxVal: 15000,
     answer: slider.value,
   },
   {
     question: 'How energy efficient is your home?',
+    minVal: 0,
+    maxVal: 100,
     answer: slider.value,
   },
 ];
