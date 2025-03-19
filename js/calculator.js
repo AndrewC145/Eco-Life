533bd435e00fc79f6939363ee9d13e87d4ebff08
Andrew Cao <andrewcao618@gmail.com>
2025-03-19 10:32:59 -0400

    Added Questions and Type Changing Function

--- js/calculator.js
+++ js/calculator.js
@@ -2,6 +2,8 @@ const slider = document.querySelector('.slider');
 const output = document.querySelector('.output');
 const prevButton = document.querySelector('.prev-button');
 const nextButton = document.querySelector('.next-button');
+const question = document.querySelector('.question');
+const type = document.querySelector('.type');
 
 document.addEventListener('DOMContentLoaded', () => {
   showRangeValue();
@@ -29,23 +31,36 @@ function showButtons() {
   }
 }
 
+function changeType() {
+  if (count <= 2) {
+    type.textContent = 'Transportation';
+  } else if (count > 2 && count < 5) {
+    type.textContent = 'Food';
+  } else {
+    type.textContent = 'Housing';
+  }
+}
+
 function moveNext() {
   if (count < questions.length - 1) {
     count++;
+    question.textContent = questions[count].question;
     slider.value = questions[count].answer;
     showRangeValue();
   }
 
+  changeType();
   showButtons();
 }
 
 function movePrev() {
   if (count > 0) {
     count--;
+    question.textContent = questions[count].question;
     slider.value = questions[count].answer;
     showRangeValue();
   }
-
+  changeType();
   showButtons();
 }
 
@@ -65,19 +80,31 @@ const questions = [
     answer: slider.value,
   },
   {
-    question: 'How many kilometers do you drive each week?',
+    question: 'How often do you take public transportation',
     answer: slider.value,
   },
   {
-    question: 'How many kilometers do you drive each week?',
+    question: 'How often do you carpool?',
     answer: slider.value,
   },
   {
-    question: 'How many kilometers do you drive each week?',
+    question: 'How often do you eat animal-based products?',
     answer: slider.value,
   },
   {
-    question: 'How many kilometers do you drive each week?',
+    question: 'What percentage of your food is locally sourced or organic?',
+    answer: slider.value,
+  },
+  {
+    question: 'How many people live in your house?',
+    answer: slider.value,
+  },
+  {
+    question: 'What is the size of your house?',
+    answer: slider.value,
+  },
+  {
+    question: 'How energy efficient is your home?',
     answer: slider.value,
   },
 ];
